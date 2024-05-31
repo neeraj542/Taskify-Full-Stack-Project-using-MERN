ab7d0b19d91543e9fad434a1dad027aa0300a0ff
Neeraj Meena <114648043+neeraj542@users.noreply.github.com>
2024-05-31 11:24:10 +0530

    Update index.js

--- server/utils/index.js
+++ server/utils/index.js
@@ -1,55 +1,25 @@
 
+
 import jwt from "jsonwebtoken";
 
 const createJWT = (res, userId) => {
-  // Create the JWT token with the user ID and the JWT secret
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "1d",
   });
 
-  // Set the cookie with the token in the HTTP response
+  // res.cookie("token", token, {
+  //   httpOnly: true,
+  //   secure: process.env.NODE_ENV !== "development", // Use secure cookies in production
+  //   sameSite: "strict", // Prevent CSRF attacks
+  //   maxAge: 1 * 24 * 60 * 60 * 1000, // 1 days
+  // });
+
   res.cookie("token", token, {
-    httpOnly: true, // Make the cookie accessible only via HTTP(S) requests
-    secure: process.env.NODE_ENV === "production", // Use secure cookies only in production
-    sameSite: "Lax", // Set the sameSite attribute to prevent CSRF attacks
-    maxAge: 1 * 24 * 60 * 60 * 1000, // Set the cookie to expire in one day
-  });
+       httpOnly: true,
+       secure: true,
+      sameSite: "none",
+      maxAge: 1 * 24 * 60 * 60 * 1000,
+ });
 };
 
 export default createJWT;
-
-
-
-
-
-
-
-
-
-
-
-// ------------------------------------------>
-
-// import jwt from "jsonwebtoken";
-
-// const createJWT = (res, userId) => {
-//   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-//     expiresIn: "1d",
-//   });
-
-//   // res.cookie("token", token, {
-//   //   httpOnly: true,
-//   //   secure: process.env.NODE_ENV !== "development", // Use secure cookies in production
-//   //   sameSite: "strict", // Prevent CSRF attacks
-//   //   maxAge: 1 * 24 * 60 * 60 * 1000, // 1 days
-//   // });
-
-//   res.cookie("token", token, {
-//        httpOnly: true,
-//        secure: true,
-//       sameSite: "none",
-//       maxAge: 1 * 24 * 60 * 60 * 1000,
-//  });
-// };
-
-// export default createJWT;
